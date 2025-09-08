4490c75f0f6e0ba2b2a9e55a73bae23ef422fef7
Adrian-Koh <adriankoh216@yahoo.com>
2025-09-08 17:35:32 +0800

    Add user prisma queries

---
+++ backend/db/usersQueries.js
@@ -0,0 +1,72 @@
+const prisma = require("./prismaClient");
+
+async function addUser(username, passwordHash, photoUrl = null, bio = null) {
+  try {
+    const user = await prisma.user.create({
+      data: {
+        username,
+        passwordHash,
+        photoUrl,
+        bio,
+      },
+    });
+    return user;
+  } catch (err) {
+    throw err;
+  }
+}
+
+async function getUserById(userid) {
+  try {
+    const user = await prisma.user.findUnique({
+      where: {
+        id: Number(userid),
+      },
+    });
+    return user;
+  } catch (err) {
+    throw err;
+  }
+}
+
+async function getUserByUsername(username) {
+  try {
+    const user = await prisma.user.findUnique({
+      where: {
+        username: username,
+      },
+    });
+    return user;
+  } catch (err) {
+    throw err;
+  }
+}
+
+async function updateUserInfo(userid, updateUserInfo) {
+  try {
+    if (!updateUserInfo) {
+      throw new Error("User info is null");
+    }
+    const username = updateUserInfo.username ? updateUserInfo.username : null;
+    const passwordHash = updateUserInfo.passwordHash
+      ? updateUserInfo.passwordHash
+      : null;
+    const photoUrl = updateUserInfo.photoUrl ? updateUserInfo.photoUrl : null;
+    const bio = updateUserInfo.bio ? updateUserInfo.bio : null;
+
+    const user = await prisma.user.update({
+      where: { id: Number(userid) },
+      data: {
+        username,
+        passwordHash,
+        photoUrl,
+        bio,
+      },
+    });
+    return user;
+  } catch (err) {
+    throw err;
+  }
+}
+
+module.exports = { addUser, getUserById, getUserByUsername, updateUserInfo };
