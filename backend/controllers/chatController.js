b6ddef1081dab23f58871556492b43c9e3067ec2
Adrian-Koh <adriankoh216@yahoo.com>
2025-09-11 13:20:51 +0800

    Implement get & post chats in prisma & controllers

--- backend/controllers/chatController.js
+++ backend/controllers/chatController.js
@@ -1,15 +1,17 @@
 const { addOnlineUser } = require("../utils/redis");
 const jwt = require("jsonwebtoken");
+const chatQueries = require("../db/chatQueries");
+const usersQueries = require("../db/usersQueries");
 
-const chatGet = async (req, res, next) => {
+const chatGet = (req, res, next) => {
   jwt.verify(req.token, process.env.SECRET_KEY, async (err, authData) => {
     if (err) {
       // it's ok to not be logged in
       res.json({ message: "not logged in" });
     } else {
-      const userId = authData.user.username;
+      const username = authData.user.username;
 
-      if (userId) {
+      if (username) {
         await addOnlineUser(authData.user.username);
       }
       res.json({ message: "successfully logged in" });
@@ -17,4 +19,51 @@ const chatGet = async (req, res, next) => {
   });
 };
 
-module.exports = { chatGet };
+const chatWithUserGet = (req, res, next) => {
+  jwt.verify(req.token, process.env.SECRET_KEY, async (err, authData) => {
+    if (err) {
+      return next(err);
+    } else {
+      const { userId } = req.params;
+      const loggedInUser = authData.user;
+
+      if (loggedInUser.username) {
+        await addOnlineUser(loggedInUser.username);
+      }
+
+      const chats = await chatQueries.getChatsBetweenUsers(
+        loggedInUser.id,
+        userId
+      );
+      chats.sort((a, b) => a.id - b.id);
+
+      res.json({ chats });
+    }
+  });
+};
+
+const chatPost = (req, res, next) => {
+  jwt.verify(req.token, process.env.SECRET_KEY, async (err, authData) => {
+    if (err) {
+      return next(err);
+    } else {
+      const { userId } = req.params;
+      const { message } = req.body;
+      const loggedInUser = authData.user;
+
+      if (loggedInUser.username) {
+        await addOnlineUser(loggedInUser.username);
+      }
+
+      const createdMessage = await chatQueries.addMessage(
+        loggedInUser.id,
+        userId,
+        message
+      );
+
+      res.json({ createdMessage });
+    }
+  });
+};
+
+module.exports = { chatGet, chatWithUserGet, chatPost };
--- backend/db/chatQueries.js
+++ backend/db/chatQueries.js
@@ -1,3 +1,44 @@
 const prisma = require("./prismaClient");
 
-module.exports = {};
+const getChatsBetweenUsers = async (userOneId, userTwoId) => {
+  try {
+    const chats = await prisma.message.findMany({
+      where: {
+        OR: [
+          {
+            AND: [
+              { senderId: Number(userOneId) },
+              { receiverId: Number(userTwoId) },
+            ],
+          },
+          {
+            AND: [
+              { receiverId: Number(userOneId) },
+              { senderId: Number(userTwoId) },
+            ],
+          },
+        ],
+      },
+    });
+    return chats;
+  } catch (err) {
+    throw err;
+  }
+};
+
+const addMessage = async (senderId, receiverId, message) => {
+  try {
+    const createdMessage = await prisma.message.create({
+      data: {
+        senderId: Number(senderId),
+        receiverId: Number(receiverId),
+        text: message,
+      },
+    });
+    return createdMessage;
+  } catch (err) {
+    throw err;
+  }
+};
+
+module.exports = { getChatsBetweenUsers, addMessage };
