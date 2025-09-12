ebacecfa8a5334ac702d12e56e402da6dd163321
Adrian-Koh <adriankoh216@yahoo.com>
2025-09-12 14:26:48 +0800

    Add info and edit buttons to user profile

--- frontend/src/components/Profile/Profile.jsx
+++ frontend/src/components/Profile/Profile.jsx
@@ -1,7 +1,35 @@
+import { useOutletContext } from "react-router-dom";
 import styles from "./Profile.module.css";
 
 const Profile = () => {
-  return <div>Profile</div>;
+  const { user } = useOutletContext();
+
+  return (
+    <div className={styles.container}>
+      {user ? (
+        <div className={styles.info}>
+          {user.photoUrl ? (
+            <>
+              <img
+                src={user.photoUrl}
+                className={styles.profilePic}
+                alt="profile pic"
+              />
+              <img src="/pencil.svg" alt="edit" className={styles.editIcon} />
+            </>
+          ) : null}
+          <div className={styles.username}>{user.username}</div>
+          <div className={styles.bio}>
+            Bio: "<i>{user.bio}</i>"
+          </div>
+          <img src="/pencil.svg" alt="edit" className={styles.editIcon} />
+          <div>Joined on: {new Date(user.joinDate).toDateString()}</div>
+        </div>
+      ) : (
+        <h1 className={styles.loggedOutMessage}>You are not logged in.</h1>
+      )}
+    </div>
+  );
 };
 
 export { Profile };
